38b099740714779ab04cd3a1793228b68c4e46ae
NotRewd <48103943+NotRewd@users.noreply.github.com>
2026-01-01 17:41:10 +0100

    add ThemeProvider component and integrate it into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import InitialRoute from "./routes/initial-route.tsx";
+import { ThemeProvider } from "@/components/providers/theme-provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <main className="h-dvh flex flex-col">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<InitialRoute />} />
-        </Routes>
-      </BrowserRouter>
-    </main>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <main className="h-dvh flex flex-col">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<InitialRoute />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
+    </ThemeProvider>
   </React.StrictMode>,
 );
