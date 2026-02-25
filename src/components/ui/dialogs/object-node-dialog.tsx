53279cae89ddfe58f4190812974edcaced942263
NotRewd <48103943+NotRewd@users.noreply.github.com>
2026-02-25 13:54:35 +0100

    feat: implement tabbed interface for object node dialog with general tab for properties

--- src/components/ui/dialogs/object-node-dialog.tsx
+++ src/components/ui/dialogs/object-node-dialog.tsx
@@ -6,10 +6,8 @@ import {
 } from "../responsive-dialog";
 import { DialogDescription, DialogTitle } from "../dialog";
 import { ObjectNodeData } from "@/components/nodes/object-node";
-import { Field, FieldContent, FieldLabel } from "../field";
-import { Textarea } from "../textarea";
-import { Input } from "../input";
-import { Switch } from "../switch";
+import { Tabs, TabsList, TabsTrigger } from "../tabs";
+import GeneralTab from "../tabs/object-node/general";
 
 interface ObjectNodeDialogProps {
   open: boolean;
@@ -22,68 +20,33 @@ const ObjectNodeDialog: FC<ObjectNodeDialogProps> = ({
   onOpenChange,
   data,
 }) => {
-  const [name, setName] = useState<string>(data.name);
-  const [stereotype, setStereotype] = useState<string>(data.stereotype || "");
-  const [abstract, setAbstract] = useState<boolean>(data.abstract || false);
-  const [note, setNote] = useState<string>(data.note || "");
+  const [internalData, setInternalData] = useState<ObjectNodeData>(data);
 
   return (
-    <ResponsiveDialog open={open} onOpenChange={onOpenChange}>
-      <ResponsiveDialogHeader>
-        <DialogTitle>{data.name}</DialogTitle>
-        <DialogDescription>
-          Customize the properties and methods of the object node.
-        </DialogDescription>
-      </ResponsiveDialogHeader>
-      <ResponsiveDialogContent>
-        <div className="flex flex-col gap-4">
-          <Field className="gap-1">
-            <FieldContent className="flex flex-col gap-0">
-              <FieldLabel>Name</FieldLabel>
-            </FieldContent>
-            <Input
-              className="font-mono"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              variant="small"
-            />
-          </Field>
-          <Field className="gap-1">
-            <FieldContent className="flex flex-col gap-0">
-              <FieldLabel>Stereotype</FieldLabel>
-            </FieldContent>
-            <Input
-              className="font-mono"
-              value={stereotype}
-              onChange={(e) => setStereotype(e.target.value)}
-              placeholder="None"
-              variant="small"
-            />
-          </Field>
-          <Field orientation="horizontal">
-            <FieldContent className="flex flex-col gap-0 flex-1">
-              <FieldLabel>Abstract</FieldLabel>
-            </FieldContent>
-            <Switch
-              checked={abstract}
-              onCheckedChange={(checked) => setAbstract(checked)}
-            />
-          </Field>
-          <Field className="mb-2 gap-1">
-            <FieldContent className="flex flex-col gap-0">
-              <FieldLabel>Note</FieldLabel>
-            </FieldContent>
-            <Textarea
-              className="font-mono min-h-25"
-              value={note}
-              onChange={(e) => setNote(e.target.value)}
-              placeholder="None"
-              variant="small"
-            />
-          </Field>
-        </div>
-      </ResponsiveDialogContent>
-    </ResponsiveDialog>
+    <Tabs defaultValue="general">
+      <ResponsiveDialog
+        open={open}
+        onOpenChange={onOpenChange}
+        className="gap-2"
+      >
+        <ResponsiveDialogHeader>
+          <DialogTitle>{data.name}</DialogTitle>
+          <DialogDescription>
+            Customize the properties and methods of the object node.
+          </DialogDescription>
+          <TabsList>
+            <TabsTrigger value="general">General</TabsTrigger>
+            <TabsTrigger value="attributes">Attributes</TabsTrigger>
+            <TabsTrigger value="methods">Methods</TabsTrigger>
+          </TabsList>
+        </ResponsiveDialogHeader>
+        <ResponsiveDialogContent>
+          <div className="flex flex-col pb-2">
+            <GeneralTab data={internalData} setData={setInternalData} />
+          </div>
+        </ResponsiveDialogContent>
+      </ResponsiveDialog>
+    </Tabs>
   );
 };
 
